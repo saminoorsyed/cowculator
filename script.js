f2ea4e3f438669999ad2e6a139218cd36535d023
saminoorsyed <saminoorsyed@gmail.com>
2022-08-26 19:28:08 -0400

    Wrote selectors and listeners for all elements
    Used hotkeys to write all the lines at once... felt cool about it

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = document.getElementById('input');
+const del = document.getElementById('delete');
+const clear = document.getElementById('clear');
+const parenthesis = document.getElementById('parenthesis');
+const exponent = document.getElementById('exponent');
+const percent = document.getElementById('percent');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const subtract = document.getElementById('subtract');
+const add = document.getElementById('add');
+const compute = document.getElementById('compute');
+
+
+
+
+
+input.addEventListener('onclick',inputfunc);
+del.addEventListener('onclick',delfunc);
+clear.addEventListener('onclick',clearfunc);
+parenthesis.addEventListener('onclick',parenthesisfunc);
+exponent.addEventListener('onclick',exponentfunc);
+percent.addEventListener('onclick',percentfunc);
+divide.addEventListener('onclick',dividefunc);
+multiply.addEventListener('onclick',multiplyfunc);
+subtract.addEventListener('onclick',subtractfunc);
+add.addEventListener('onclick',addfunc);
+compute.addEventListener('onclick',computefunc);
