f7d15b0ecd350a8c24c1c7cc30522854f460024a
saminoorsyed <saminoorsyed@gmail.com>
2022-08-27 19:11:49 -0400

    Implement pushInput methodology
    joins consecutive numbers input into a single number and pushes to a
    list with operators in separate index. Uses RegEx.

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ const screen = document.getElementById('input');
 // global vars
 
 let input = [];
-let operation;
+let numbers = [];
+const operation = [];
 let result;
 // functions
 
+// push the input into an array
 function pushInput(ins) {
-    input.push(ins);
-    console.log(input);
-    display();
+    // check if the value is a number, if so, concat it to the previous number
+    const re = new RegExp('[0-9]');
+    if (re.test(ins) || ins === '.' ){
+        numbers += ins;
+        console.log(numbers);
+    // if the value is not a number, push the concatenated number to the input list
+    }else{
+        // cannot put two operators in a row
+        if (numbers.length <=0){
+            alert('you cannot insert two operators in a row!');
+            return;
+        }
+        input.push(numbers);
+        // reset number value to an empty list for the next number to be input
+        numbers = []
+        // push the operator into the list
+        input.push(ins);
+        console.log(input);
+    }
 }
 
+function compute(){
+
+}
 function display(){
     screen.textContent = input.join('');
+    console.log(input);
 }
 // listen for key codes to press buttons
 window.addEventListener('keyup', (e)=>{
