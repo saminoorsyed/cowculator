11970f4178f7760302ee92711102888f4dc8a4b5
saminoorsyed <saminoorsyed@gmail.com>
2022-08-27 18:30:06 -0400

    Added too much in one commit
    buttons respond to key events
        --listen on window and match to data-button values
    button values are pushed to an input array for storage.
        --global variable
    input is displayed on screen as a string:
        --used .join to turn all elements of array to one string

--- script.js
+++ script.js
@@ -1,76 +1,31 @@
-// element selectors
-const input = document.getElementById('input');
-const del = document.getElementById('delete');
-const clear = document.getElementById('clear');
-const parenthesis = document.getElementById('parenthesis');
-const exponent = document.getElementById('exponent');
-const percent = document.getElementById('percent');
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const subtract = document.getElementById('subtract');
-const add = document.getElementById('add');
-const compute = document.getElementById('compute');
+// query selectors
+const buttons = document.querySelectorAll('[data-button]')
+const screen = document.getElementById('input');
 
+// global vars
+
+let input = [];
+let operation;
+let result;
 // functions
 
-
-
-function inputfunc () {
-
+function pushInput(ins) {
+    input.push(ins);
+    console.log(input);
+    display();
 }
 
-function delfunc () {
-
+function display(){
+    screen.textContent = input.join('');
 }
-
-function clearfunc () {
-
-}
-
-function parenthesisfunc () {
-
-}
-
-function exponentfunc () {
-
-}
-
-function percentfunc () {
-
-}
-
-function dividefunc () {
-
-}
-
-function multiplyfunc () {
-
-}
-
-function subtractfunc () {
-
-}
-
-function addfunc () {
-
-}
-
-function computefunc () {
-
-}
-
-
-
-
-// event listeners
-input.addEventListener('onclick',inputfunc);
-del.addEventListener('onclick',delfunc);
-clear.addEventListener('onclick',clearfunc);
-parenthesis.addEventListener('onclick',parenthesisfunc);
-exponent.addEventListener('onclick',exponentfunc);
-percent.addEventListener('onclick',percentfunc);
-divide.addEventListener('onclick',dividefunc);
-multiply.addEventListener('onclick',multiplyfunc);
-subtract.addEventListener('onclick',subtractfunc);
-add.addEventListener('onclick',addfunc);
-compute.addEventListener('onclick',computefunc);
+// listen for key codes to press buttons
+window.addEventListener('keyup', (e)=>{
+    console.log(e.key);
+    buttons.forEach(butt =>{
+        if (butt.dataset.button === e.key){
+            butt.click();
+            butt.classList.add('active');
+            setTimeout(()=>butt.classList.remove('active'),200);
+        }
+    });
+});
